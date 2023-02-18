0a83b24f9681bd1deb9ef0d465f6b445102fadda
nelllyai <worldpiece000@gmail.com>
2023-02-18 20:59:51 +0300

    fix svg component

--- src/UI/Svg/Svg.jsx
+++ src/UI/Svg/Svg.jsx
@@ -1,19 +1,24 @@
-import style from './Svg.module.css';
+// import style from './Svg.module.css';
 import PropTypes from 'prop-types';
-import {useEffect, useState} from 'react';
 
-export const Svg = ({path}) => {
-  console.log(path);
-  const [element, setElement] = useState(null);
+export const Svg = ({name}) => {
+  let directory = '';
 
-  useEffect(() => {
-    import(path).then(res => {
-      const Icon = res.ReactComponent;
-      setElement(<Icon className={style.svg} />);
-    });
-  }, []);
+  if (name.includes('--')) {
+    const parts = name.split('--');
+    directory = `${parts[0]}/`;
+    name = parts[1];
+  }
 
-  return element;
+  let icon = require(`../../assets/icons/${directory}${name}.svg?sprite`);
+
+  if (Object.prototype.hasOwnProperty.call(icon, 'default')) {
+    icon = icon.default;
+  }
+
+  icon.href = `#${icon.id}`;
+
+  return icon;
 };
 
 Svg.propTypes = {
